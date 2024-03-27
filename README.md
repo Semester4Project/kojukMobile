7320850a4b68001c48ac59b37aae8ab10bdd27b2
Adi Hendra <adihendra1303@gmail.com>
2024-03-27 14:07:24 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mobile_kojuk
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/mobile_kojuk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_kojuk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
