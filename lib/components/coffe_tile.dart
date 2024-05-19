619bb2b2d1593ea0b5b99bc1d93410f56cb3a29e
intanpuspitasari20 <intanpst20@gmail.com>
2024-05-19 21:13:01 +0700

    update dashboard

--- lib/components/coffe_tile.dart
+++ lib/components/coffe_tile.dart
@@ -1,90 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:kojuk_mobile/models/coffe.dart';
 
-class CoffeTile extends StatelessWidget{
-  Coffe coffe;
-  CoffeTile({super.key, required this.coffe});
+class CoffeTile extends StatelessWidget {
+  final Coffe coffe;
+  CoffeTile({Key? key, required this.coffe});
 
   @override
-  Widget build(BuildContext context){
-    return Container(
-      margin: EdgeInsets.only(left: 25),
-      width: 280,
-      decoration: BoxDecoration(
-        color: Colors.grey[100],
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        children: [
-          //coffe pic
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.asset(coffe.image,
-              width: 300, // Adjust width as needed
-              height: 330, // Adjust height as needed
-              fit: BoxFit.cover,
-          
-            ),
-            
-          ),
-          //description
-          Text(
-            coffe.description,
-            style: TextStyle(color: Colors.grey[600]),
-          ),
-
-          //price +detail
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //coffe name
-                    Text(
-                      coffe.name, 
-                      style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20
-                     ),
-                     ),
-            
-                     const SizedBox(height: 2),
-            
-                    //price
-                    Text(
-                      'Rp.' + coffe.price,
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                      ),
-                  ],
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // Handle navigation to coffee detail page here
+        
+      },
+      child: Container(
+        margin: EdgeInsets.only(left: 25, top: 10, bottom: 10),
+        width: 280,
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Coffee picture
+            Flexible(
+              flex: 2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
                 ),
-            
-                //cart button
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      bottomRight: Radius.circular(12),
+                child: Image.asset(
+                  coffe.image,
+                  fit: BoxFit.cover, // Fit the image within the container
+                  width: double.infinity,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Description
+                  Text(
+                    coffe.description,
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                  SizedBox(height: 8),
+                  // Coffee name
+                  Text(
+                    coffe.name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
                     ),
                   ),
-                  child: const Icon(
-                    Icons.shopping_cart,
-                    color: Colors.white,
+                  SizedBox(height: 5),
+                  // Price and cart button
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Rp.' + coffe.price,
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: const BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                        ),
+                        child: const Icon(
+                          Icons.shopping_cart,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          )
-
-          //button to cart
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -14,100 +14,100 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard'),
+        title: const Text('Dashboard'),
         actions: [
           IconButton(
-            icon: Icon(Icons.shopping_cart),
+            icon: const Icon(Icons.shopping_cart_outlined),
             onPressed: () {
               // Add your shopping cart action here
             },
           ),
         ],
       ),
-      body: Column(
-        children: [
-          // Search bar
-          Container(
-            padding: EdgeInsets.all(12),
-            margin: const EdgeInsets.symmetric(horizontal: 25),
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Text(
-                  'search',
-                  style: TextStyle(color: Colors.grey),
-                ),
-                Icon(
-                  Icons.search,
-                  color: Colors.grey,
-                ),
-              ],
-            ),
-          ),
-
-          // Message
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 25.0),
-            child: Text(
-              'everyone flies.. some fly longer than others',
-              style: TextStyle(color: Colors.grey[600]),
-            ),
-          ),
-
-          // Hot picks
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: const [
-                Text(
-                  'Hot Picks 🔥',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Search bar
+            Container(
+              padding: const EdgeInsets.all(12),
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    'Search',
+                    style: TextStyle(color: Colors.grey),
                   ),
-                ),
-                Text(
-                  'See all',
-                  style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
-                )
-              ],
+                  Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(height: 10),
-
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true, // Add this line
-              itemCount: 4,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                // Create coffee
-                Coffe coffee = Coffe(
-                  name: 'Robusta jawa',
-                  price: '500000',
-                  image: 'assets/images/coffe.jpg',
-                  description: 'Robusta',
-                );
-                return CoffeTile(
-                  coffe: coffee,
-                );
-              },
+            // Message
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20),
+              child: Text(
+                'Everyone flies... some fly longer than others.',
+                style: TextStyle(color: Colors.grey[600]),
+              ),
             ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(top: 25.0, left: 25, right: 25),
-            child: Divider(
-              color: Colors.white,
+            // Hot picks
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: const [
+                  Text(
+                    'Hot Picks 🔥',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  Text(
+                    'See all',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 450, // Adjust the height as needed
+              child: ListView.builder(
+                itemCount: 4,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  // Create coffee
+                  Coffe coffee = Coffe(
+                    name: 'Robusta Jawa',
+                    price: '500000',
+                    image: 'assets/images/coffe2.jpg',
+                    description: 'Robusta',
+                  );
+                  return CoffeTile(coffe: coffee);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+              child: Divider(
+                color: Colors.grey[300],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
