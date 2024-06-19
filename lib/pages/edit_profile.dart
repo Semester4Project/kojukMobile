5529b014aba933166df567b123ce2dbfda469bc9
intanpuspitasari20 <intanpst20@gmail.com>
2024-06-19 14:50:23 +0700

    get user and update view profile

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class EditProfile extends StatefulWidget {
   final File? profileImage;
   final String username;
   final String email;
-  final String address;
-  final String phoneNumber;
   final Function(File?, String, String, String, String) onProfileUpdated;
 
   EditProfile({
     required this.profileImage,
     required this.username,
     required this.email,
-    required this.address,
-    required this.phoneNumber,
     required this.onProfileUpdated,
   });
 
@@ -27,8 +24,6 @@ class _EditProfileState extends State<EditProfile> {
   final _formKey = GlobalKey<FormState>();
   late String _username;
   late String _email;
-  late String _address;
-  late String _phoneNumber;
   File? _image;
 
   final ImagePicker _picker = ImagePicker();
@@ -38,8 +33,6 @@ class _EditProfileState extends State<EditProfile> {
     super.initState();
     _username = widget.username;
     _email = widget.email;
-    _address = widget.address;
-    _phoneNumber = widget.phoneNumber;
     _image = widget.profileImage;
   }
 
@@ -52,13 +45,21 @@ class _EditProfileState extends State<EditProfile> {
     }
   }
 
-  void _saveProfile() {
+  Future<void> _saveProfile() async {
     if (_formKey.currentState?.validate() ?? false) {
       _formKey.currentState?.save();
-      widget.onProfileUpdated(_image, _username, _email, _address, _phoneNumber);
+      
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      if (_image != null) {
+        await prefs.setString('profile_picture', _image!.path);
+      }
+      await prefs.setString('username', _username);
+      await prefs.setString('email', _email);
+
+      widget.onProfileUpdated(_image, _username, _email, '', '');
       Navigator.pop(context);
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Profile berhasil di perbarui!')),
+        SnackBar(content: Text('Profile berhasil diperbarui!')),
       );
     }
   }
@@ -77,55 +78,57 @@ class _EditProfileState extends State<EditProfile> {
           child: ListView(
             children: [
               Center(
-                child: Stack(
-                  children: [
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundImage: _image != null
-                          ? FileImage(_image!)
-                          : AssetImage('assets/images/kucing.jpg') as ImageProvider,
-                    ),
-                    Positioned(
-                      right: 4,
-                      bottom: 4,
-                      child: InkWell(
-                        onTap: () {
-                          showModalBottomSheet(
-                            context: context,
-                            builder: (context) {
-                              return SafeArea(
-                                child: Wrap(
-                                  children: <Widget>[
-                                    ListTile(
-                                      leading: Icon(Icons.photo_library),
-                                      title: Text('Gallery'),
-                                      onTap: () {
-                                        _pickImage(ImageSource.gallery);
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                    ListTile(
-                                      leading: Icon(Icons.photo_camera),
-                                      title: Text('Camera'),
-                                      onTap: () {
-                                        _pickImage(ImageSource.camera);
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                          );
-                        },
-                        child: CircleAvatar(
-                          radius: 15,
-                          backgroundColor: Colors.brown[800],
-                          child: Icon(Icons.edit, size: 15, color: Colors.white),
+                child: RepaintBoundary(
+                  child: Stack(
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundImage: _image != null
+                            ? FileImage(_image!)
+                            : AssetImage('assets/images/profile1.jpg') as ImageProvider,
+                      ),
+                      Positioned(
+                        right: 4,
+                        bottom: 4,
+                        child: GestureDetector(
+                          onTap: () {
+                            showModalBottomSheet(
+                              context: context,
+                              builder: (context) {
+                                return SafeArea(
+                                  child: Wrap(
+                                    children: <Widget>[
+                                      ListTile(
+                                        leading: Icon(Icons.photo_library),
+                                        title: Text('Gallery'),
+                                        onTap: () async {
+                                          await _pickImage(ImageSource.gallery);
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      ListTile(
+                                        leading: Icon(Icons.photo_camera),
+                                        title: Text('Camera'),
+                                        onTap: () async {
+                                          await _pickImage(ImageSource.camera);
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                          child: CircleAvatar(
+                            radius: 15,
+                            backgroundColor: Colors.brown[800],
+                            child: Icon(Icons.edit, size: 15, color: Colors.white),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(height: 16),
@@ -164,42 +167,6 @@ class _EditProfileState extends State<EditProfile> {
                   _email = value!;
                 },
               ),
-              const SizedBox(height: 16),
-              TextFormField(
-                initialValue: _address,
-                decoration: InputDecoration(
-                  labelText: 'Alamat',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Masukkan alamat';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _address = value!;
-                },
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                initialValue: _phoneNumber,
-                decoration: InputDecoration(
-                  labelText: 'Phone Number',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a phone number';
-                  } else if (!RegExp(r'^\d+$').hasMatch(value)) {
-                    return 'Please enter a valid phone number';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _phoneNumber = value!;
-                },
-              ),
               const SizedBox(height: 32),
               ElevatedButton(
                 onPressed: _saveProfile,
@@ -207,7 +174,7 @@ class _EditProfileState extends State<EditProfile> {
                   backgroundColor: Colors.brown[700],
                 ),
                 child: const Text(
-                  'Simpan Peruban',
+                  'Simpan Perubahan',
                   style: TextStyle(color: Colors.white),
                 ),
               ),
@@ -218,4 +185,3 @@ class _EditProfileState extends State<EditProfile> {
     );
   }
 }
- 
--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:kojuk_mobile/pages/keranjang.dart';
-import 'package:kojuk_mobile/pages/daftar_transaksi.dart';
-import 'package:kojuk_mobile/pages/edit_profile.dart';
+import 'package:http/http.dart' as http;
+import 'package:kojuk_mobile/pages/alamat_page.dart';
+import 'dart:convert';
 import 'dart:io';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'keranjang.dart';
+import 'daftar_transaksi.dart';
+import 'edit_profile.dart';
+import 'login_page.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -11,10 +16,61 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   File? _profileImage;
-  String _username = 'Intan Puspitasari';
-  String _email = 'intan@gmail.com';
-  String _address = 'Jl. Sudirman No. 123';
-  String _phoneNumber = '081234567890';
+  String _username = '';
+  String _email = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _username = prefs.getString('username') ?? 'No Name';
+      _email = prefs.getString('email') ?? 'No Email';
+      String? profileImagePath = prefs.getString('profile_picture');
+      if (profileImagePath != null && profileImagePath.isNotEmpty) {
+        _profileImage = File(profileImagePath);
+      }
+    });
+  }
+
+  Future<void> logout(BuildContext context) async {
+    final url = 'http://127.0.0.1:8000/api/auth/logout';
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer YOUR_TOKEN_HERE',  // Sesuaikan token jika dibutuhkan
+      },
+    );
+
+    if (response.statusCode == 200) {
+      // Hapus data pengguna dari SharedPreferences
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.clear();
+
+      Navigator.of(context).pushReplacementNamed('/login');
+    } else {
+      final errorResponse = json.decode(response.body);
+      print('Logout failed: ${errorResponse['message']}');
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Logout Failed'),
+          content: Text(errorResponse['message']),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +78,7 @@ class _ProfileState extends State<Profile> {
       appBar: AppBar(
         backgroundColor: Colors.brown[700],
         title: const Text('Profile'),
+        automaticallyImplyLeading: false,
         actions: [
           IconButton(
             icon: const Icon(Icons.shopping_cart_outlined),
@@ -39,115 +96,134 @@ class _ProfileState extends State<Profile> {
       body: Container(
         width: double.infinity,
         color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Stack(
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: _profileImage != null
-                        ? FileImage(_profileImage!)
-                        : AssetImage('assets/images/kucing.jpg') as ImageProvider,
-                  ),
-                  Positioned(
-                    right: 4,
-                    bottom: 4,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => EditProfile(
-                              profileImage: _profileImage,
-                              username: _username,
-                              email: _email,
-                              address: _address,
-                              phoneNumber: _phoneNumber,
-                              onProfileUpdated: (File? image, String username, String email, String address, String phoneNumber) {
-                                setState(() {
-                                  _profileImage = image;
-                                  _username = username;
-                                  _email = email;
-                                  _address = address;
-                                  _phoneNumber = phoneNumber;
-                                });
-                              },
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: _profileImage != null
+                          ? FileImage(_profileImage!)
+                          : AssetImage('assets/images/profile1.jpg') as ImageProvider,
+                    ),
+                    Positioned(
+                      right: 4,
+                      bottom: 4,
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => EditProfile(
+                                profileImage: _profileImage,
+                                username: _username,
+                                email: _email,
+                                onProfileUpdated: (File? image, String username, String email, String _, String __) async {
+                                  SharedPreferences prefs = await SharedPreferences.getInstance();
+                                  setState(() {
+                                    _profileImage = image;
+                                    _username = username;
+                                    _email = email;
+                                  });
+                                  if (image != null) {
+                                    await prefs.setString('profile_picture', image.path);
+                                  }
+                                  await prefs.setString('username', username);
+                                  await prefs.setString('email', email);
+                                },
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                      child: CircleAvatar(
-                        radius: 15,
-                        backgroundColor: Colors.brown[800],
-                        child: Icon(Icons.edit, size: 15, color: Colors.white),
+                          );
+                        },
+                        child: CircleAvatar(
+                          radius: 15,
+                          backgroundColor: Colors.brown[800],
+                          child: Icon(Icons.edit, size: 15, color: Colors.white),
+                        ),
                       ),
                     ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  _username,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Text(
-                _username,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
                 ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                _email,
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
+                const SizedBox(height: 8),
+                Text(
+                  _email,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[600],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 32),
-              Divider(color: Colors.brown[800]),
-              ListTile(
-                leading: const Icon(Icons.list_alt, color: Colors.brown),
-                title: const Text('Daftar Transaksi'),
-                trailing: IconButton(
-                  icon: Icon(Icons.arrow_forward_ios, color: Colors.brown),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DaftarTransaksi(),
-                      ),
-                    );
+                const SizedBox(height: 32),
+                Divider(color: Colors.brown[800]),
+                ListTile(
+                  leading: const Icon(Icons.list_alt, color: Colors.brown),
+                  title: const Text('Daftar Transaksi'),
+                  trailing: IconButton(
+                    icon: Icon(Icons.arrow_forward_ios, color: Colors.brown),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DaftarTransaksi(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Divider(color: Colors.brown[800]),
+                ListTile(
+                  leading: const Icon(Icons.shopping_cart, color: Colors.brown),
+                  title: const Text('Keranjang'),
+                  trailing: IconButton(
+                    icon: Icon(Icons.arrow_forward_ios, color: Colors.brown),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Keranjang(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Divider(color: Colors.brown[800]),
+                ListTile(
+                  leading: const Icon(Icons.location_on, color: Colors.brown),
+                  title: const Text('Alamat'),
+                  trailing: IconButton(
+                    icon: Icon(Icons.arrow_forward_ios, color: Colors.brown),
+                    onPressed: () {
+                      /*Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AlamatPage(),
+                        ),
+                      );*/// Navigate to address management screen (to be implemented)
+                    },
+                  ),
+                ),
+                Divider(color: Colors.brown[800]),
+                ListTile(
+                  leading: const Icon(Icons.exit_to_app, color: Colors.brown),
+                  title: const Text('Sign Out'),
+                  onTap: () {
+                    logout(context);
                   },
                 ),
-              ),
-              Divider(color: Colors.brown[800]),
-              ListTile(
-                leading: const Icon(Icons.shopping_cart, color: Colors.brown),
-                title: const Text('Keranjang'),
-                trailing: IconButton(
-                  icon: Icon(Icons.arrow_forward_ios, color: Colors.brown),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Keranjang(),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Divider(color: Colors.brown[800]),
-              ListTile(
-                leading: const Icon(Icons.exit_to_app, color: Colors.brown),
-                title: const Text('Sign Out'),
-                onTap: () {
-                  // Handle sign out
-                },
-              ),
-              Divider(color: Colors.brown[800]),
-            ],
+                Divider(color: Colors.brown[800]),
+              ],
+            ),
           ),
         ),
       ),
